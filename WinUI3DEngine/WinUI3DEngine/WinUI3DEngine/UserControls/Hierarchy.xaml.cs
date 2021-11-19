81b0cbc342c3160604e3839f44899ebbc2324357
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-11-19 09:48:14 +0100

    Enabled Hierarchy Functionality

--- WinUI3DEngine/WinUI3DEngine/WinUI3DEngine/UserControls/Hierarchy.xaml.cs
+++ WinUI3DEngine/WinUI3DEngine/WinUI3DEngine/UserControls/Hierarchy.xaml.cs
@@ -39,61 +39,61 @@ namespace WinUI3DEngine.UserControls
 
         void Initialize(object sender, RoutedEventArgs e)
         {
-            //var engineObjectList = CMain.Singleton.m_Layout.m_ViewPort.m_renderer.m_scene.m_objectManager.m_list;
-            //engineObjectList.OnAdd += list_OnAdd;
+            var engineObjectList = CMain.Singleton.m_Layout.m_ViewPort.m_Engine.m_Scene.m_ObjectManager.m_List;
+            engineObjectList.OnAdd += list_OnAdd;
 
-            //CScene scene = new CScene();
-            //foreach (var item in engineObjectList)
-            //{
-            //    var newEntry = new TreeEntry() { Object = item, Name = item.m_name, ID = item.ID };
-            //    if (item.m_parent != null)
-            //        newEntry.IDparent = item.m_parent.ID;
+            CScene scene = new CScene();
+            foreach (var item in engineObjectList)
+            {
+                var newEntry = new TreeEntry() { Object = item, Name = item.m_Name, ID = item.ID };
+                if (item.m_Parent != null)
+                    newEntry.IDparent = item.m_Parent.ID;
 
-            //    scene.m_Hierarchy.Add(newEntry);
-            //}
+                scene.m_Hierarchy.Add(newEntry);
+            }
 
-            //foreach (var item in scene.m_Hierarchy)
-            //    item.Node = new Microsoft.UI.Xaml.Controls.TreeViewNode() { Content = item.Name, IsExpanded = true };
+            foreach (var item in scene.m_Hierarchy)
+                item.Node = new TreeViewNode() { Content = item.Name, IsExpanded = true };
 
-            //TreeEntry tmp;
-            //foreach (var item in scene.m_Hierarchy)
-            //    if ((tmp = scene.GetParent(item)) != null)
-            //    {
-            //        if (!x_TreeView_Hierarchy.RootNodes.Contains(tmp.Node))
-            //            x_TreeView_Hierarchy.RootNodes.Add(tmp.Node);
+            TreeEntry tmp;
+            foreach (var item in scene.m_Hierarchy)
+                if ((tmp = scene.GetParent(item)) != null)
+                {
+                    if (!x_TreeView_Hierarchy.RootNodes.Contains(tmp.Node))
+                        x_TreeView_Hierarchy.RootNodes.Add(tmp.Node);
 
-            //        if (!tmp.Node.Children.Contains(item.Node))
-            //            foreach (var child in scene.GetChildren(tmp))
-            //                tmp.Node.Children.Add(child.Node);
-            //    }
-            //    else if (!x_TreeView_Hierarchy.RootNodes.Contains(item.Node))
-            //        x_TreeView_Hierarchy.RootNodes.Add(item.Node);
+                    if (!tmp.Node.Children.Contains(item.Node))
+                        foreach (var child in scene.GetChildren(tmp))
+                            tmp.Node.Children.Add(child.Node);
+                }
+                else if (!x_TreeView_Hierarchy.RootNodes.Contains(item.Node))
+                    x_TreeView_Hierarchy.RootNodes.Add(item.Node);
         }
 
         void list_OnAdd(object sender, EventArgs e)
         {
-            //var engineObjectList = m_main.m_Layout.m_ViewPort.m_renderer.m_scene.m_objectManager.m_list;
+            var engineObjectList = CMain.Singleton.m_Layout.m_ViewPort.m_Engine.m_Scene.m_ObjectManager.m_List;
 
-            //CScene scene = new CScene();
+            CScene scene = new CScene();
 
-            //var newObject = engineObjectList[engineObjectList.Count - 1];
-            //var newEntry = new TreeEntry() { Object = newObject, Name = newObject.m_name, ID = newObject.ID };
-            //if (newObject.m_parent != null)
-            //    newEntry.IDparent = newObject.m_parent.ID;
+            var newObject = engineObjectList[engineObjectList.Count - 1];
+            var newEntry = new TreeEntry() { Object = newObject, Name = newObject.m_Name, ID = newObject.ID };
+            if (newObject.m_Parent != null)
+                newEntry.IDparent = newObject.m_Parent.ID;
 
-            //newEntry.Node = new Microsoft.UI.Xaml.Controls.TreeViewNode() { Content = newEntry.Name, IsExpanded = true };
-            //scene.m_Hierarchy.Add(newEntry);
+            newEntry.Node = new TreeViewNode() { Content = newEntry.Name, IsExpanded = true };
+            scene.m_Hierarchy.Add(newEntry);
 
-            //TreeEntry tmp;
-            //if ((tmp = scene.GetParent(newEntry)) != null)
-            //{
-            //    if (!x_TreeView_Hierarchy.RootNodes.Contains(tmp.Node))
-            //        x_TreeView_Hierarchy.RootNodes.Add(tmp.Node);
+            TreeEntry tmp;
+            if ((tmp = scene.GetParent(newEntry)) != null)
+            {
+                if (!x_TreeView_Hierarchy.RootNodes.Contains(tmp.Node))
+                    x_TreeView_Hierarchy.RootNodes.Add(tmp.Node);
 
-            //    tmp.Node.Children.Add(newEntry.Node);
-            //}
-            //else if (!x_TreeView_Hierarchy.RootNodes.Contains(newEntry.Node))
-            //    x_TreeView_Hierarchy.RootNodes.Add(newEntry.Node);
+                tmp.Node.Children.Add(newEntry.Node);
+            }
+            else if (!x_TreeView_Hierarchy.RootNodes.Contains(newEntry.Node))
+                x_TreeView_Hierarchy.RootNodes.Add(newEntry.Node);
         }
     }
 }
